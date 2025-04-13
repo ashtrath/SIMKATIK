3f971b2d0301dceb0143cf70b3e1e556dc2f0a9d
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2025-04-13 13:37:25 +0700

    feat(CurrencyInput): add prefix prop, disable negatives

--- src/components/composites/CurrencyInput/index.tsx
+++ src/components/composites/CurrencyInput/index.tsx
@@ -1,14 +1,19 @@
 import * as CurrencyInputPrimitive from "react-currency-input-field";
 import { Input } from "~/components/ui/Input";
 
-const CurrencyInput = ({ ...props }: CurrencyInputPrimitive.CurrencyInputProps) => {
+interface CurrencyInputProps extends CurrencyInputPrimitive.CurrencyInputProps {
+    prefix?: string;
+}
+
+const CurrencyInput = ({ prefix = "Rp.", ...props }: CurrencyInputProps) => {
     return (
         <div className="group relative w-full">
             <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-2.5">
-                <span className="font-medium text-sm">Rp.</span>
+                <span className="font-medium text-muted-foreground text-sm">{prefix}</span>
             </div>
             <CurrencyInputPrimitive.CurrencyInput
                 intlConfig={{ locale: "id-ID" }}
+                allowNegativeValue={false}
                 allowDecimals={false}
                 customInput={Input}
                 inputMode="decimal"
